70f941e29104657e00ff54443da358131adae28b
Achmad Kurnianto <achmad_kurnianto@yahoo.co.id>
2025-02-17 08:06:29 +0700

    chore(configuration): :wrench: update configuration files
    - update `tailwindcss` into the latest major version
    - update vite to generate files with `zstd` compression
    - update `react-router` configuration to support experimental optimize dependencies

--- react-router.config.ts
+++ react-router.config.ts
@@ -5,4 +5,7 @@ export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   // ssr: false,
   prerender: ['/404', '/500'],
+  future: {
+    unstable_optimizeDeps: true
+  }
 } satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,22 @@
 import { reactRouter } from '@react-router/dev/vite'
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import tailwindcssVite from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import { compression } from 'vite-plugin-compression2'
 import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { compress } from '@mongodb-js/zstd'
 
 import 'dotenv/config'
 
 export default defineConfig(({ command }) => ({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [tailwindcssPostcss],
     },
   },
   plugins: [
+    tailwindcssVite(),
     reactRouter(),
     tsconfigPaths(),
     ...(command === 'build'
@@ -23,6 +26,16 @@ export default defineConfig(({ command }) => ({
             algorithm: 'brotliCompress',
             filename: '[path][base].br',
           }),
+          compression({
+            async algorithm (buf, options) {
+              // @ts-expect-error
+              return await compress(buf, options.level)
+            },
+            compressionOptions: {
+              level: 20
+            },
+            filename: '[path][base].zst',
+          })
         ]
       : []),
   ],
